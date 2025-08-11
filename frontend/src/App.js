b9af90c8fe61f4028e912fb1da41b894a7bf7c76
vai <yshak2002@gmail.com>
2025-08-11 14:48:06 -0400

    Convert frontend from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+
+const URL = "http://localhost:8000/readPDF";
+
+export default function App(){
+  const [file, setFile] = useState(null);
+  const [res, setRes] = useState(null);
+  const [err, setErr] = useState(""); 
+
+
+  async function handleUpload(uploadEvent) {
+    uploadEvent.preventDefault();
+    setErr("");
+    setRes(null);
+
+    if(!file){
+      setErr("Please, Upload a file");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file)
+
+    try{
+      const response = await fetch(URL, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok){
+        throw new Error(await response.text());
+      }
+
+      const data = await response.json();
+
+      setRes(data);
+
+    }
+    catch (error){
+      setErr(error.message || "something went wrong");
+    } 
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleUpload}>
+        <input 
+        type="file" accept="application/pdf" 
+        onChange={(e) => setFile(e.target.files[0])} />
+        <button type="submit">Upload</button>
+      </form>
+      {err && <p style={{ color: "red" }}>Error: {err}</p>}
+
+      {res && (
+        <div>
+          <h2>
+            {res.fileName}
+          </h2>
+          <pre>
+            {res.pageCount}
+          </pre>
+        {res.pages && res.pages.map((p) => (
+            <div key={p.pageNumber}>
+              <h3>Page {p.pageNumber + 1}</h3>
+              <pre style={{ whiteSpace: "pre-wrap" }}>{p.text}</pre>
+            </div>
+          ))}
+        </div>
+      )}
+
+    </div>
+  );
+}
